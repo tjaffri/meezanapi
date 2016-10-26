7d5509399439b9253acb58a2e0b19e47834bb43f
Taqi Jaffri <tjaffri@gmail.com>
2016-10-26 12:45:57 -0700

    temporarily print stack traces in production

--- src/server.js
+++ src/server.js
@@ -88,7 +88,7 @@ if (server.get('env') === 'development') {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {},
+      error: err, // {}
     });
   });
 }
